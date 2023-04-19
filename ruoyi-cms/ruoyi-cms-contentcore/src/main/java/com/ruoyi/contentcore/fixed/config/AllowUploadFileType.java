e8582213583302ee5ebcd130d40796c40345d986
兮玥 <190785909@qq.com>
2023-04-19 09:51:58 +0800

    国际化补充

--- ruoyi-cms/ruoyi-cms-contentcore/src/main/java/com/ruoyi/contentcore/fixed/config/AllowUploadFileType.java
+++ ruoyi-cms/ruoyi-cms-contentcore/src/main/java/com/ruoyi/contentcore/fixed/config/AllowUploadFileType.java
@@ -17,23 +17,22 @@ import com.ruoyi.system.service.ISysConfigService;
 public class AllowUploadFileType extends FixedConfig {
 
 	public static final String ID = "AllowUploadFileType";
-	
+
 	private static final ISysConfigService configService = SpringUtils.getBean(ISysConfigService.class);
-	
+
 	/**
 	 * 文件管理默认允许上传的文件类型
 	 */
-	public static final List<String> ALLOWED_UPLOAD_EXTENSION = List.of(
-            "bmp", "gif", "jpg", "jpeg", "png", "webp", "psd", "ai", "tif", "tiff",
-            "doc", "docx", "xls", "xlsx", "ppt", "pptx", "pdf", "fla", "swf", "js", "css", "shtml", "html", "htm", "txt", "ttf", "eot",
-            "mp4", "avi", "rmvb", "mpg", "flv", "mpeg", "rm", "mov", "wmv", "wmp", "mp3", "wma", "wav", "ogg",
-            "rar", "zip", "gz", "bz2", "z", "iso", "cab", "jar"
-		);
+	public static final List<String> ALLOWED_UPLOAD_EXTENSION = List.of("bmp", "gif", "jpg", "jpeg", "png", "webp",
+			"psd", "ai", "tif", "tiff", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "pdf", "fla", "swf", "js", "css",
+			"shtml", "html", "htm", "txt", "ttf", "eot", "mp4", "avi", "rmvb", "mpg", "flv", "mpeg", "rm", "mov", "wmv",
+			"wmp", "mp3", "wma", "wav", "ogg", "rar", "zip", "gz", "bz2", "z", "iso", "cab", "jar");
 
 	public AllowUploadFileType() {
-		super(ID, "{CONFIG." + ID + "}", StringUtils.EMPTY, null);
+		super(ID, "{CONFIG." + ID + "}", StringUtils.EMPTY,
+				"默认支持：bmp,gif,jpg,jpeg,png,webp,psd,ai,tif,tiff,doc,docx,xls,xlsx,ppt,pptx,pdf,fla,swf,js,css,shtml,html,htm,txt,ttf,eot,mp4,avi,rmvb,mpg,flv,mpeg,rm,mov,wmv,wmp,mp3,wma,wav,ogg,rar,zip,gz,bz2,z,iso,cab,jar");
 	}
-	
+
 	/**
 	 * 指定文件名或后缀名是否允许上传的文件类型
 	 * 
