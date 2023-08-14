e7bca41046737bb21cc8d972033f189ce6ddc0d1
兮玥 <190785909@qq.com>
2023-08-14 10:48:10 +0800

    修正后台内容删除错误，移除application.yml测试用配置。

--- ruoyi-cms/ruoyi-cms-member/src/main/java/com/ruoyi/cms/member/listener/MemberListener.java
+++ ruoyi-cms/ruoyi-cms-member/src/main/java/com/ruoyi/cms/member/listener/MemberListener.java
@@ -11,7 +11,6 @@ import com.ruoyi.contentcore.listener.event.AfterContentSaveEvent;
 import com.ruoyi.contentcore.service.IContentService;
 import com.ruoyi.contentcore.service.impl.ContentDynamicDataService;
 import com.ruoyi.member.listener.event.*;
-import com.ruoyi.member.security.StpMemberUtil;
 import com.ruoyi.member.service.IMemberStatDataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
@@ -21,73 +20,75 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class MemberListener {
 
-	private final IContentService contentService;
+    private final IContentService contentService;
 
-	private final ContentDynamicDataService contentDynamicDataService;
+    private final ContentDynamicDataService contentDynamicDataService;
 
-	private final IMemberStatDataService memberStatDataService;
+    private final IMemberStatDataService memberStatDataService;
 
-	@EventListener
-	public void afterContentSaveEvent(AfterContentSaveEvent event) {
-		if (event.isAdd() && IdUtils.validate(event.getContent().getContentEntity().getContributorId())) {
-			// 更新会员文章数量
-			memberStatDataService.changeMemberStatData(StpMemberUtil.getLoginIdAsLong(), ContributeMemberStatData.TYPE, 1);
-		}
-	}
+    @EventListener
+    public void afterContentSaveEvent(AfterContentSaveEvent event) {
+        Long contributorId = event.getContent().getContentEntity().getContributorId();
+        if (event.isAdd() && IdUtils.validate(contributorId)) {
+            // 更新会员文章数量
+            memberStatDataService.changeMemberStatData(contributorId, ContributeMemberStatData.TYPE, 1);
+        }
+    }
 
-	@EventListener
-	public void afterContentDeleteEvent(AfterContentDeleteEvent event) {
-		if (IdUtils.validate(event.getContent().getContentEntity().getContributorId())) {
-			// 更新会员文章数量
-			memberStatDataService.changeMemberStatData(StpMemberUtil.getLoginIdAsLong(), ContributeMemberStatData.TYPE, -1);
-		}
-	}
+    @EventListener
+    public void afterContentDeleteEvent(AfterContentDeleteEvent event) {
+        Long contributorId = event.getContent().getContentEntity().getContributorId();
+        if (IdUtils.validate(contributorId)) {
+            // 更新会员文章数量
+            memberStatDataService.changeMemberStatData(contributorId, ContributeMemberStatData.TYPE, -1);
+        }
+    }
 
-	@EventListener
-	public void beforeMemberFavorite(BeforeMemberFavoriteEvent event) {
-		if (CmsMemberConstants.MEMBER_FAVORITES_DATA_TYPE.equals(event.getDataType())) {
-			CmsContent content = this.contentService.getById(event.getDataId());
-			Assert.notNull(content, () -> CommonErrorCode.DATA_NOT_FOUND_BY_ID.exception("dataId", event.getDataId()));
-		}
-	}
+    @EventListener
+    public void beforeMemberFavorite(BeforeMemberFavoriteEvent event) {
+        if (CmsMemberConstants.MEMBER_FAVORITES_DATA_TYPE.equals(event.getDataType())) {
+            CmsContent content = this.contentService.getById(event.getDataId());
+            Assert.notNull(content, () -> CommonErrorCode.DATA_NOT_FOUND_BY_ID.exception("dataId", event.getDataId()));
+        }
+    }
 
-	@EventListener
-	public void afterMemberFavorite(AfterMemberFavoriteEvent event) {
-		if (CmsMemberConstants.MEMBER_FAVORITES_DATA_TYPE.equals(event.getMemberFavorites().getDataType())) {
-			// 内容收藏数+1
-			this.contentDynamicDataService.increaseFavoriteCount(event.getMemberFavorites().getDataId());
-		}
-	}
+    @EventListener
+    public void afterMemberFavorite(AfterMemberFavoriteEvent event) {
+        if (CmsMemberConstants.MEMBER_FAVORITES_DATA_TYPE.equals(event.getMemberFavorites().getDataType())) {
+            // 内容收藏数+1
+            this.contentDynamicDataService.increaseFavoriteCount(event.getMemberFavorites().getDataId());
+        }
+    }
 
-	@EventListener
-	public void afterCancelMemberFavorite(AfterMemberCancelFavoriteEvent event) {
-		if (CmsMemberConstants.MEMBER_FAVORITES_DATA_TYPE.equals(event.getMemberFavorites().getDataType())) {
-			// 内容收藏数-1
-			this.contentDynamicDataService.decreaseFavoriteCount(event.getMemberFavorites().getDataId());
-		}
-	}
+    @EventListener
+    public void afterCancelMemberFavorite(AfterMemberCancelFavoriteEvent event) {
+        if (CmsMemberConstants.MEMBER_FAVORITES_DATA_TYPE.equals(event.getMemberFavorites().getDataType())) {
+            // 内容收藏数-1
+            this.contentDynamicDataService.decreaseFavoriteCount(event.getMemberFavorites().getDataId());
+        }
+    }
 
-	@EventListener
-	public void beforeMemberLike(BeforeMemberLikeEvent event) {
-		if (CmsMemberConstants.MEMBER_LIKE_DATA_TYPE.equals(event.getDataType())) {
-			CmsContent content = this.contentService.getById(event.getDataId());
-			Assert.notNull(content, () -> CommonErrorCode.DATA_NOT_FOUND_BY_ID.exception("dataId", event.getDataId()));
-		}
-	}
+    @EventListener
+    public void beforeMemberLike(BeforeMemberLikeEvent event) {
+        if (CmsMemberConstants.MEMBER_LIKE_DATA_TYPE.equals(event.getDataType())) {
+            CmsContent content = this.contentService.getById(event.getDataId());
+            Assert.notNull(content, () -> CommonErrorCode.DATA_NOT_FOUND_BY_ID.exception("dataId", event.getDataId()));
+        }
+    }
 
-	@EventListener
-	public void afterMemberLike(AfterMemberLikeEvent event) {
-		if (CmsMemberConstants.MEMBER_LIKE_DATA_TYPE.equals(event.getMemberLike().getDataType())) {
-			// 内容点赞数+1
-			this.contentDynamicDataService.increaseLikeCount(event.getMemberLike().getDataId());
-		}
-	}
+    @EventListener
+    public void afterMemberLike(AfterMemberLikeEvent event) {
+        if (CmsMemberConstants.MEMBER_LIKE_DATA_TYPE.equals(event.getMemberLike().getDataType())) {
+            // 内容点赞数+1
+            this.contentDynamicDataService.increaseLikeCount(event.getMemberLike().getDataId());
+        }
+    }
 
-	@EventListener
-	public void afterCancelMemberFavorite(AfterMemberCancelLikeEvent event) {
-		if (CmsMemberConstants.MEMBER_LIKE_DATA_TYPE.equals(event.getMemberLike().getDataType())) {
-			// 内容点赞数-1
-			this.contentDynamicDataService.decreaseLikeCount(event.getMemberLike().getDataId());
-		}
-	}
+    @EventListener
+    public void afterCancelMemberFavorite(AfterMemberCancelLikeEvent event) {
+        if (CmsMemberConstants.MEMBER_LIKE_DATA_TYPE.equals(event.getMemberLike().getDataType())) {
+            // 内容点赞数-1
+            this.contentDynamicDataService.decreaseLikeCount(event.getMemberLike().getDataId());
+        }
+    }
 }
