a9dfb536334df35fb52e58a57e34e47ec8344eda
lc <1035709119@qq.com>
2023-08-17 10:24:42 +0800

    删除捐赠

--- ruoyi-ui/src/views/index.vue
+++ ruoyi-ui/src/views/index.vue
@@ -12,38 +12,6 @@
       <el-col :span="12">
         <shortcut></shortcut>
         <cms-site-data-stat></cms-site-data-stat>
-        <el-card shadow="hover" class="mt10">
-          <div slot="header" class="clearfix">
-            <span>捐赠支持</span>
-          </div>
-          <div class="body">
-            <el-row :gutter="20">
-              <el-col :span="12" style="text-align:center;">
-                <div class="mb10">微信</div>
-                <el-image
-                  style="height: 200px"
-                  :src="require('@/assets/images/wxpay.png')"
-                  fit="cover">
-                </el-image>
-                <div style="color: #909399;font-size:12px;line-height: 30px;">
-                  向 无弦(**仪) 捐赠
-                </div>
-              </el-col>
-              <el-col :span="12" style="text-align:center;">
-                <div class="mb10">支付宝</div>
-                <el-image
-                  style="height: 200px"
-                  :src="require('@/assets/images/alipay.png')"
-                  fit="cover">
-                </el-image>
-                <div style="color: #909399;font-size:12px;line-height: 30px;">
-                  向 无弦(**仪) 捐赠
-                </div>
-              </el-col>
-            </el-row>
-            <div style="display: inline-block; height: 30px; line-height: 30px">你可以请作者喝杯咖啡表示鼓励^_^</div>
-          </div>
-        </el-card>
       </el-col>
     </el-row>
   </div>
